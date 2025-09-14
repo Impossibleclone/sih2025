4940147b0a34cb52014324c720810690f92a3d0b
shivjeet1 <shivamlavhalepatil@gmail.com>
2025-09-14 17:52:40 +0530

    removed ui

--- ui/src-tauri/src/main.rs
+++
@@ -1,9 +0,0 @@
-fn main() {
-    tauri::Builder::default()
-        .plugin(tauri_plugin_fs::init())
-        .plugin(tauri_plugin_dialog::init())
-        .plugin(tauri_plugin_log::Builder::new().build())
-        .plugin(tauri_plugin_shell::init())
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
-}
